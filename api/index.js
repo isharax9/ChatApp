23994f804d894084439fa3566729e8bc817696dc
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-10-03 22:29:48 +0530

    Created Connection with MongoDB

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+const jwt = require("jsonwebtoken");
+
+mongoose
+  .connect("mongodb+srv://macknight9889:s3fogbjefLKCq9RF@cluster0.nrhf4nz.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Mongo Db");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDb", err);
+  });
+
+app.listen(port, () => {
+  console.log("Server running on port 8000");
+});
+
