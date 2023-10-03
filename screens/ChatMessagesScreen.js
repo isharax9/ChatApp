36880a528871770b9851c115788565edd1d695a8
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-10-03 23:01:46 +0530

    Screens are Added
    📌add main screens
    📌add stack navigator
    📌all working so far fine 💯

---
+++ screens/ChatMessagesScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ChatMessagesScreen = () => {
+  return (
+    <View>
+      <Text>ChatMessagesScreen</Text>
+    </View>
+  )
+}
+
+export default ChatMessagesScreen
+
+const styles = StyleSheet.create({})
