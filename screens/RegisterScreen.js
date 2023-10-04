20c195e31c7e001741b7e8d53313ea414a202333
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-10-04 06:06:59 +0530

    Registration and working fine with MongoDB
    📌Revert "Added Registration & Login"
    
    This reverts commit a1d1e64a76b90e8428866746a4919b9639601aae.
    
    📌 replace localhost with 10.0.2.2

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -25,9 +25,9 @@ import {
         image: image,
       };
   
-      //send a POST  request to the backend API to register the user
+      // send a POST  request to the backend API to register the user
       axios
-        .post("http://localhost:8000/register", user)
+        .post("http://10.0.2.2:8000/register", user)
         .then((response) => {
           console.log(response);
           Alert.alert(
